a4b56bdefb931b6457116671fbac708ffc1f1107
putri indrawati rohma <putriir2002@gmail.com>
2021-04-09 22:40:29 +0700

    Kasih LSP

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::query()->create([
+            'name' => 'Putri',
+            'email' => 'admin@admin.com',
+            'password' => Hash::make('qwertyui'),
+        ]);
+
+        $this->call(ProductSeeder::class);
+    }
+}
